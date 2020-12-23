1f5114a09f497bec382ccddf7894419184c052c5
dorothy <1303815571@qq.com>
2020-12-23 14:32:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myproject/ui/weather/WeatherFragment.kt
@@ -0,0 +1,32 @@
+package com.example.myproject.ui.weather
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myproject.R
+
+class WeatherFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = WeatherFragment()
+    }
+
+    private lateinit var viewModel: WeatherViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(WeatherViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/myproject/ui/weather/WeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myproject.ui.weather
+
+import androidx.lifecycle.ViewModel
+
+class WeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
